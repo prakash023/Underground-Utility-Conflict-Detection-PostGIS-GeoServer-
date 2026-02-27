03253bdcd9c530800be78fad2b921df6b6f227db
prakash023 <prakash023@gmail.com>
2026-02-27 08:40:12 +0100

    Legende Risk/conflict level

--- script.js
+++ script.js
@@ -79,50 +79,129 @@ L.control.layers(null, {
 
 // Legend has been removed
 
-map.on('click', function (e) {
 
-    var url = utilitiesLayer.getFeatureInfoUrl(
-        e.latlng,
-        map.getZoom(),
-        {
-            info_format: 'application/json'
-        }
-    );
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.features.length > 0) {
-                var props = data.features[0].properties;
-
-                L.popup()
-                    .setLatLng(e.latlng)
-                    .setContent(
-                        "<b>Project:</b> " + props.project_name + "<br>" +
-                        "<b>Utility:</b> " + props.utility_type
-                    )
-                    .openOn(map);
-            }
-        });
-});
 //Conflicts layer for clickable and those are WFS, so we need to fetch them separately 
 // and add as GeoJSON layer
-fetch("http://localhost:8081/geoserver/leipzig/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=leipzig:fiber_network&outputFormat=application/json")
+// ================= CONSTRUCTION RISK (WFS) =================
+fetch("http://localhost:8081/geoserver/leipzig/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=leipzig:construction_with_risk&outputFormat=application/json&srsName=EPSG:4326")
 .then(res => res.json())
 .then(data => {
 
-    var fiberLayer = L.geoJSON(data, {
-        style: {
-            color: "blue",
-            weight: 2
-        },
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup(
-                "<b>Utility:</b> Fiber<br>" +
-                "<b>Depth:</b> " + feature.properties.depth + " m<br>" +
-                "<b>Installed:</b> " + feature.properties.installation_year
-            );
+    console.log("Construction Features:", data.features.length);
+
+    var constructionLayer = L.geoJSON(data, {
+
+        style: function(feature) {
+            return {
+                color: "#000000",
+                fillColor: "#00ff00",
+                fillOpacity: 0.8,
+                weight: 2
+            };
         }
+
     }).addTo(map);
 
+    // FORCE zoom to data
+    map.fitBounds(constructionLayer.getBounds());
+
 });
+
+
+
+// ================= CONFLICT HOTSPOTS (WFS) =================
+fetch("http://localhost:8081/geoserver/leipzig/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=leipzig:utility_conflicts&outputFormat=application/json&srsName=EPSG:4326")
+.then(res => res.json())
+.then(data => {
+
+    console.log("Conflict Features:", data.features.length);
+
+    var conflictLayer = L.geoJSON(null, {
+
+        onEachFeature: function(feature, layer) {
+
+            layer.bindPopup(
+                "<b>Project:</b> " + feature.properties.project_name + "<br>" +
+                "<b>Utility:</b> " + feature.properties.utility_type + "<br>" +
+                "<b>Utility Depth:</b> " + feature.properties.utility_depth + " m<br>" +
+                "<b>Excavation Depth:</b> " + feature.properties.excavation_depth_m + " m<br>" +
+                "<b>Risk Level:</b> " + feature.properties.risk_level
+            );
+
+            layer.on("click", function() {
+                let originalRadius = layer.getRadius();
+                layer.setRadius(14);
+                setTimeout(function() {
+                    layer.setRadius(originalRadius);
+                }, 600);
+            });
+        }
+
+    });
+
+    // Convert each polygon to centroid circle marker
+    data.features.forEach(function(feature) {
+
+        let coords = feature.geometry.coordinates[0][0];
+
+        // Calculate centroid manually using Leaflet
+        let polygon = L.polygon(feature.geometry.coordinates[0]);
+        let center = polygon.getBounds().getCenter();
+
+        let risk = feature.properties.risk_level;
+
+        let fillColor = "#ffff00";
+        if (risk === "HIGH") fillColor = "#ff0000";
+        if (risk === "MEDIUM") fillColor = "#ff9900";
+
+        let marker = L.circleMarker(center, {
+            radius: 8,
+            fillColor: fillColor,
+            color: "#000000",
+            weight: 2,
+            opacity: 1,
+            fillOpacity: 1
+        });
+
+        marker.feature = feature;
+        conflictLayer.addLayer(marker);
+
+    });
+
+    conflictLayer.addTo(map);
+    conflictLayer.bringToFront();
+
+});
+
+// ================= LEGEND =================
+var legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function () {
+
+    var div = L.DomUtil.create("div", "info legend");
+
+    div.innerHTML = `
+        <div style="
+            background: white;
+            padding: 10px 12px;
+            border-radius: 8px;
+            box-shadow: 0 0 15px rgba(0,0,0,0.2);
+            font-size: 14px;
+            line-height: 20px;
+        ">
+            <b>Project Risk</b><br>
+            <span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#8b0000;margin-right:6px;"></span> Critical<br>
+            <span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#ff8c00;margin-right:6px;"></span> Moderate<br>
+            <span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#2ecc71;margin-right:6px;"></span> Low<br>
+            <hr style="margin:6px 0;">
+            <b>Conflict Level</b><br>
+            <span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#ff0000;margin-right:6px;"></span> High<br>
+            <span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#ffa500;margin-right:6px;"></span> Medium<br>
+            <span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#ffff00;margin-right:6px;"></span> Low
+        </div>
+    `;
+
+    return div;
+};
+
+legend.addTo(map);
